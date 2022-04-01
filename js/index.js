e4bff0c85e43ccbf3acb51c8bcb64f9b9d54e85a
Antonio <antonio.junior@original.io>
2022-03-31 22:41:13 -0300

    Criação das funções para filtrar e montar os blocos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const url = "http://www.mocky.io/v2/5bc3b9cc30000012007586b7";
+let globalData;
+const section = document.querySelector(".all-cards");
 
 window.addEventListener("load", () => {
   fetch(url)
@@ -6,13 +8,26 @@ window.addEventListener("load", () => {
       return res.json();
     })
     .then(data => {
-      console.log(data);
+      console.log(data.reverse());
       generateBlocks(data);
+      globalData = data;
     });
+
+  const allPromos = document.querySelector(".all-promos");
+  allPromos.addEventListener("click", () => {
+    while (section.firstChild) {
+      section.removeChild(section.lastChild);
+    }
+    generateBlocks(globalData);
+  });
+
+  const newCustomersFilter = document.querySelector(".new-customers");
+  newCustomersFilter.addEventListener("click", () => {
+    filterBtns(globalData);
+  });
 });
 
 const generateBlocks = (data) => {
-  const section = document.querySelector(".all-cards");
 
   data.map(d => {
     const newDiv = document.createElement("div");
@@ -32,16 +47,77 @@ const generateBlocks = (data) => {
     newTxt.innerHTML = d.description;
     newDiv.appendChild(newTxt);
 
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("btn-container");
+    newDiv.appendChild(buttonsDiv);
+
     const btnTerms = document.createElement("button");
     btnTerms.classList.add("bt-terms");
     btnTerms.innerHTML = "Terms & Conditions";
-    newDiv.appendChild(btnTerms);
+    buttonsDiv.appendChild(btnTerms);
+
 
     const btnJoin = document.createElement("button");
     btnJoin.classList.add("btn-join");
     btnJoin.innerHTML = "Join Now";
-    newDiv.appendChild(btnJoin);
+    buttonsDiv.appendChild(btnJoin);
 
     section.appendChild(newDiv);
+
   })
+}
+
+let newCustomers = [];
+
+const filterBtns = () => {
+  let section = document.querySelector(".all-cards");
+
+  while (section.firstChild) {
+    section.removeChild(section.lastChild);
+  }
+
+  newCustomers = globalData.filter(d => {
+    if (d.onlyNewCustomers === true) {
+      return d.id;
+    }
+  });
+  console.log(newCustomers);
+
+  newCustomers.forEach(n => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("promotions");
+
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", "https://via.placeholder.com/500x300");
+    newDiv.appendChild(newImg);
+
+    const newHeading = document.createElement("h2");
+    newHeading.classList.add("heading");
+    newHeading.innerHTML = n.name;
+    newDiv.appendChild(newHeading);
+
+    const newTxt = document.createElement("p");
+    newTxt.classList.add("desc-txt");
+    newTxt.innerHTML = n.description;
+    newDiv.appendChild(newTxt);
+
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("btn-container");
+    newDiv.appendChild(buttonsDiv);
+
+    const btnTerms = document.createElement("button");
+    btnTerms.classList.add("bt-terms");
+    btnTerms.innerHTML = "Terms & Conditions";
+    buttonsDiv.appendChild(btnTerms);
+
+
+    const btnJoin = document.createElement("button");
+    btnJoin.classList.add("btn-join");
+    btnJoin.innerHTML = "Join Now";
+    buttonsDiv.appendChild(btnJoin);
+
+    section.appendChild(newDiv);
+  });
+
+  newCustomers = [];
 }
